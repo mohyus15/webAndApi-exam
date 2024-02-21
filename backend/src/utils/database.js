2675a5222676e4131ee041525141465792ca414b
mohyus15 <mohyus15@hotmail.com>
2024-02-21 02:28:29 +0100

    added and fixed some error

--- backend/src/utils/database.js
+++ backend/src/utils/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongo_url = 'mongodb+srv://mohyus20:OGH2RHHNWhlZXHeQ@db.6tlp7p3.mongodb.net/webAndApi?retryWrites=true&w=majority';
+const mongo_url = process.env.mongo_url;
 mongoose.connection.once('open', async () => {
     console.log('MongoDB connection is ready');
 });
@@ -9,11 +9,9 @@ mongoose.connection.on('error', async err => {
 });
 
 const mongooseConnect = async () => {
-    await mongoose.connect(
-       mongo_url
-    );
+    await mongoose.connect("mongodb+srv://mohyus20:OGH2RHHNWhlZXHeQ@db.6tlp7p3.mongodb.net/webAndApi?retryWrites=true&w=majority");
 
-
+    
     mongoose.set('strictQuery', true);
 };
 
